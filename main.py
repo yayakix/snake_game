c69e02ace356305e1b9e570eb658d2a2378f94b2
Iyana Marquez <yayakix@gmail.com>
2022-07-31 14:04:53 -0400

    snake controlled by keys

--- main.py
+++ main.py
@@ -9,11 +9,21 @@ screen.tracer(0)
 
 
 snake = Snake()
+screen.listen()
+
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down,'Down')
+screen.onkey(snake.left,'Left')
+screen.onkey(snake.right,'Right')
+
+
+
 game_on = True
 while game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
 
     # game_on = False
 
--- snake.py
+++ snake.py
@@ -14,6 +14,17 @@ class Snake:
             newsquare.goto(self.x_cor, 0)
             self.all_turtles.append(newsquare)
             self.x_cor -= 20
+    def up(self):
+        self.all_turtles[0].setheading(90)
+    def down(self):
+        self.all_turtles[0].setheading(270)
+
+    def left(self):
+        self.all_turtles[0].setheading(180)
+
+    def right(self):
+        self.all_turtles[0].setheading(0)
+
 
     def move(self):
         for x in range(len(self.all_turtles) - 1, 0, -1):
@@ -21,3 +32,4 @@ class Snake:
             new_y = self.all_turtles[x - 1].ycor()
             self.all_turtles[x].goto(new_x, new_y)
         self.all_turtles[0].forward(20)
+
